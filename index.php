513bbebc975afa2a4f7100736110381aa687dfa4
argentumCake <dhikarahmayadi@gmail.com>
2020-07-09 20:52:24 +0700

    nambahin form login di index

--- index.php
+++ index.php
@@ -1,14 +1,21 @@
-<?php
-  include "header.php"
-?>
-
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
     <title>Gudang</title>
+    <link rel="stylesheet" type="text/css" href="asset/css/bootstrap.css">
+    <script type="text/javascript" href="asset/js/bootstrap.js"></script>
   </head>
   <body>
-    
+    <div class="container-fluid">
+      <form action="login.php" method="get">
+        <table class="table">
+          <tr>
+            <td>Username</td>
+            <td><input type="text" name="username"></td>
+          </tr>
+        </table>
+      </form>
+    </div>
   </body>
 </html>
